a4790ff9a63409cb8b668783d6a01323ebd4fc3b
OnishYurii <onishchenkoyurii@gmail.com>
2023-11-02 16:53:44 +0200

    Updated

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,16 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_7krsDhJaLjVXQehyeACWzGu7EMwB1gQU6ebhoTFOXaWC3zqUcFWCaSzto65jBMk3';
-const header = {
-  'x-api-key': API_KEY,
+const options = {
+  headers: {
+    'x-api-key': API_KEY,
+  },
 };
 
 export function fetchBreeds() {
   const endPoint = '/breeds';
   const url = BASE_URL + endPoint;
-  return fetch(url, header).then(res => {
+  return fetch(url, options).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
     }
@@ -20,7 +22,7 @@ export function fetchCatByBreed(breedId) {
   const endPoint = '/images/search';
   const params = `?breed_ids=${breedId}`;
   const url = BASE_URL + endPoint + params;
-  return fetch(url, header).then(res => {
+  return fetch(url, options).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
     }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 
 const select = document.querySelector('.breed-select');
+const div = document.querySelector('.cat-info');
 
-console.log(fetchBreeds());
 fetchBreeds()
   .then(breeds => renderSelect(breeds))
   .catch(error => console.log(error));
@@ -11,9 +11,11 @@ fetchBreeds()
 select.addEventListener('input', e => {
   e.preventDefault();
   const breedId = select.options[select.selectedIndex].value;
-  fetchCatByBreed(breedId).then(cat => {
-    console.log(cat);
-  });
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      renderCat(cat);
+    })
+    .catch(err => console.log(err));
 });
 
 function renderSelect(breeds) {
@@ -23,4 +25,25 @@ function renderSelect(breeds) {
     })
     .join('');
   select.insertAdjacentHTML('beforeend', markup);
+}
+
+function catTemplate(cat) {
+  const image = cat[0].url;
+  const catTemperament = cat[0].breeds[0].temperament;
+  const catName = cat[0].breeds[0].name;
+  const catDescription = cat[0].breeds[0].description;
+  return `
+<div class="image-container">
+  <img src="${image}" alt="${catName}" class="cat-image" />
+</div>
+<div class="cat-body">
+  <h4 class="cat-name">${catName}</h4>
+  <p>${catDescription}</p>
+  <p class="cat-temperament"><span class="cat-temp-span">Temperament:</span> ${catTemperament}</p>
+</div>`;
+}
+
+function renderCat(cat) {
+  const markup = catTemplate(cat);
+  div.innerHTML = markup;
 }
